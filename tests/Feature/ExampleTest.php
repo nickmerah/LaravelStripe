b457900243644ec0f6ef3f94f0a2bce6bdda5d03
Full-Stack Developer | Data Analyst | Machine Learning Researcher | Expert Reviewer <91393468+nickmerah@users.noreply.github.com>
2022-11-07 23:38:49 +0100

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,19 +2,19 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    //payment method CRUD functionality
     public function test_example()
     {
-        $response = $this->get('/');
+        $user = new User(array('name' => 'John'));
+        $this->be($user);
+
+        $response = $this->get('/paymentmethod');
 
         $response->assertStatus(200);
     }
